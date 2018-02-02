c0645d1c33e1226c8c4bac22a60e4e9bca544ad4
Jiaming Deng <drkitachi@gmail.com>
2018-02-02 11:41:49 -0500

    Finish question 2

---
+++ HW1/CircleArea.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CircleArea{
+	public static void main(String[] args){
+		
+		Scanner input = new Scanner(System.in);
+		
+		double radius, areaOfCircle;
+		
+		while(input.hasNext()){
+			radius = input.nextDouble();
+			areaOfCircle = Math.pow(radius, 2) * Math.PI;
+			System.out.printf("Radius: %f   Area: %f%n", radius, areaOfCircle);
+		}
+	}
+}
