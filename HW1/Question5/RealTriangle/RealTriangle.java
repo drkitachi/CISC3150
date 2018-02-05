90b394d4e7f7575af1faed493e736a4b3a700246
Jiaming Deng <drkitachi@gmail.com>
2018-02-04 23:02:58 -0500

    added RealTriangleApp.java

--- HW1/Question5/RealTriangle/RealTriangle.java
+++ HW1/Question5/RealTriangle/RealTriangle.java
@@ -1,46 +1,43 @@
-/*	
-	Question 5: Computes the sides of a triangle and check if real or not
-	
-*/
+package RealTriangle;
 
 import java.util.Scanner;
 import RealTriangle.TwoDPoint;
 
 public class RealTriangle{
-	private static TwoDPoint a;
-	private static TwoDPoint b;
-	private static TwoDPoint c;
+	private TwoDPoint a;
+	private TwoDPoint b;
+	private TwoDPoint c;
+	private double sideAB;
+	private double sideBC;
+	private double sideCA;
 	
-	RealTriangle(){
+	public RealTriangle(){
 		a = new TwoDPoint();
 		b = new TwoDPoint();
 		c = new TwoDPoint();
 	}
 	
-	public static void main(String[] args){
-		double sideAB, sideBC, sideCA;
-		RealTriangle triangle = new RealTriangle();
+	public void run(){
+		readPoints();
 		
-		triangle.readPoints();
+		sideAB = calculateSide(a,b);
+		sideBC = calculateSide(b,c);
+		sideCA = calculateSide(c,a);
 		
-		sideAB = triangle.calculateSide(a,b);
-		sideBC = triangle.calculateSide(b,c);
-		sideCA = triangle.calculateSide(c,a);
-		
-		if(triangle.checkTriangle(sideAB,sideBC,sideCA))
+		if(checkTriangle())
 			System.out.println("This is a real triangle");
 		else
 			System.out.println("This is a fake triangle");
 		
 	}
 	
-	double calculateSide(TwoDPoint p1, TwoDPoint p2){
+	private double calculateSide(TwoDPoint p1, TwoDPoint p2){
 		double x1 = p1.getX(), y1 = p1.getY();
 		double x2 = p2.getX(), y2 = p2.getY();
 		return Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
 	}
 	
-	void readPoints(){
+	private void readPoints(){
 		Scanner input = new Scanner(System.in);
 		double x, y;
 		
@@ -63,7 +60,7 @@ public class RealTriangle{
 		c.setY(y);
 	}
 	
-	boolean checkTriangle(double sideAB, double sideBC, double sideCA){
+	private boolean checkTriangle(){
 		if((sideAB + sideBC) <= sideCA) return false;
 		if((sideBC + sideCA) <= sideAB) return false;
 		if((sideAB + sideCA) <= sideBC) return false;
---
+++ HW1/Question5/RealTriangle/RealTriangleApp.java
@@ -0,0 +1,17 @@
+/*	
+	Question 5: Computes the sides of a triangle and check if real or not
+	Command for command line: java RealTriangle.RealTriangleApp
+	
+*/
+
+package RealTriangle;
+
+import RealTriangle.RealTriangle;
+
+public class RealTriangleApp{
+	public static void main(String[] args){
+		RealTriangle triangle = new RealTriangle();
+		
+		triangle.run();
+	}
+}
